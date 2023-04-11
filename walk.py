ad53719bf5a1c2c435171c2e3c4226b8d59eb1e5
chaerynlee <chaeryn2@illinois.edu>
2023-04-11 12:54:53 -0500

    Update walk.py

--- walk.py
+++ walk.py
@@ -6,8 +6,8 @@
 #SBATCH --mem-per-cpu=4G
 #SBATCH --nice=10000
 #SBATCH --time=7-00:00
-#SPATCH --output=/data/keeling/a/chaeryn2/totally_geodesic/htlinkexterior%A_%a.out
-#SPATCH --error=/data/keeling/a/chaeryn2/totally_geodesic_error/htlinkexterior%A_%a.error
+#SBATCH --output=/data/keeling/a/chaeryn2/totally_geodesic/htlinkexterior%A_%a
+#SBATCH --error=/data/keeling/a/chaeryn2/totally_geodesic_error/htlinkexterior%A_%a
 
 import os
 import pickle
