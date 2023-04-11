ddd08a506e37e9a3f70ef7b9dd51c32276e265a8
chaerynlee <chaeryn2@illinois.edu>
2023-04-11 15:49:20 -0500

    Update results.py

--- results.py
+++ results.py
@@ -13,11 +13,11 @@ def sort_results():
         if 'totally_geodesic_info_manifold%i'%i in os.listdir('/data/keeling/a/chaeryn2/totally_geodesic/'):
             with open('/data/keeling/a/chaeryn2/totally_geodesic/totally_geodesic_info_manifold%i'%i, 'rb') as f:
                 info = pickle.load(f)
-            pd.concat([manifold_data, {'index':i,
+            pd.concat([manifold_data, pd.Dataframe({'index':i,
                                       'manifold':info['manifold'],
                                       'runtime_surfaces':info['runtime_surfaces'],
                                       'runtime_gp':info['runtime_gp'],
-                                      'num_tot_geo':len(info['tot_geo'])}])
+                                      'num_tot_geo':len(info['tot_geo'])})])
     manifold_data.to_csv('/data/keeling/a/chaeryn2/totally-geodesic/result.csv', index=False)
 
 if __name__ == '__main__':
