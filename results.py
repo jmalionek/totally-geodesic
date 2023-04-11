a1a6a3c12935373c9fd695213cfcd274eb0f4984
chaerynlee <chaeryn2@illinois.edu>
2023-04-11 15:54:45 -0500

    Update results.py

--- results.py
+++ results.py
@@ -8,16 +8,16 @@ def sort_results():
                                   'manifold':pd.Series(dtype='str'),
                                   'runtime_surfaces':pd.Series(dtype='float'),
                                   'runtime_gp':pd.Series(dtype='float'),
-                                  'num_tot_geo':pd.Series(dtype='int')})
+                                  'num_tot_geo':pd.Series(dtype='int')}, index=[0])
     for i in range(271636):
         if 'totally_geodesic_info_manifold%i'%i in os.listdir('/data/keeling/a/chaeryn2/totally_geodesic/'):
             with open('/data/keeling/a/chaeryn2/totally_geodesic/totally_geodesic_info_manifold%i'%i, 'rb') as f:
                 info = pickle.load(f)
-            pd.concat([manifold_data, pd.DataFrame({'index':i,
-                                      'manifold':info['manifold'],
-                                      'runtime_surfaces':info['runtime_surfaces'],
-                                      'runtime_gp':info['runtime_gp'],
-                                      'num_tot_geo':len(info['tot_geo'])})])
+            pd.concat([manifold_data, pd.DataFrame({'index': i,
+                                      'manifold': info['manifold'],
+                                      'runtime_surfaces': info['runtime_surfaces'],
+                                      'runtime_gp': info['runtime_gp'],
+                                      'num_tot_geo': len(info['tot_geo'])}, index=[0])])
     manifold_data.to_csv('/data/keeling/a/chaeryn2/totally-geodesic/result.csv', index=False)
 
 if __name__ == '__main__':
