8ecefeefa9fca483faacfcaa874245618c67f7a1
jmalionek <jmalionek@gmail.com>
2023-09-30 09:47:48 -0500

    fixed a data_collection bug

--- data_collection.py
+++ data_collection.py
@@ -80,7 +80,7 @@ def weekly_check():
 	for keyword in keywords:
 		print(keyword)
 		results, manifolds = get_all_results(keyword, get_manifolds = True)
-		print('number done:', len(results[0]))
+		print('number done:', len(results['tot_geo']))
 		print('"number with totally geodesic surfaces":', len([l for l in results['tot_geo'] if len(l) > 0]))
 		print('number which ACTUALLY have tot geo', len([i for i in range(len(manifolds)) if len(results['tot_geo'][i]) > 0 and manifolds[i].solution_type(enum=True) <= 2]))
 		print()
