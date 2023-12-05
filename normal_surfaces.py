d0242244e843b5eeb8b8d9dac6f31190344878bb
chaerynlee <chaeryn2@illinois.edu>
2023-12-05 12:05:12 +0900

    changed spaces(?) to tabs

--- normal_surfaces.py
+++ normal_surfaces.py
@@ -813,20 +813,20 @@ def boundary_to_surface(M):
 	return regina.NormalSurface(M, regina.NS_STANDARD, surface_vector)
 
 def find_surfaces(vertex_surfaces, e):
-    '''
-    vertex_surfaces: list of regina normal surfaces that are vertex surfaces
-    modified to return all surfaces of a given euler characteristic (not genus)
-    '''
-    NS = vertex_surfaces
-    NS_nscomplex = [surfaces.NormalSurface(S, i) for i, S in enumerate(NS) if S.eulerChar() < 0]
-    all_faces = faces.admissible_faces(NS_nscomplex)
-    all_surfaces = []
-    for AF in all_faces:
-        all_surfaces = all_surfaces + AF.surfaces_euler_in_interior(e)
-    return all_surfaces
+	'''
+	vertex_surfaces: list of regina normal surfaces that are vertex surfaces
+	modified to return all surfaces of a given euler characteristic (not genus)
+	'''
+	NS = vertex_surfaces
+	NS_nscomplex = [surfaces.NormalSurface(S, i) for i, S in enumerate(NS) if S.eulerChar() < 0]
+	all_faces = faces.admissible_faces(NS_nscomplex)
+	all_surfaces = []
+	for AF in all_faces:
+		all_surfaces = all_surfaces + AF.surfaces_euler_in_interior(e)
+	return all_surfaces
 
 def main():
-    import matplotlib.pyplot as plt
+	import matplotlib.pyplot as plt
 	import doubling
 	M = snappy.Manifold('10_135')
 	print(f'num tetrahedra: {M.num_tetrahedra()}')
@@ -1025,7 +1025,7 @@ def main6():
 		# print('connected:', SR.isConnected())
 		# print('orientable:', SR.isOrientable())
 		# print('embedded:', SR.embedded())
-	    # index of sfces with euler char < -2: [15, 17, 21, 22, 26, 27, 28, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265]
+		# index of sfces with euler char < -2: [15, 17, 21, 22, 26, 27, 28, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265]
 
 def main7():
 	"""
