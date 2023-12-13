17ed8912f7bb085f2c54a05731c6e26c887ca43d
chaerynlee <chaeryn2@illinois.edu>
2023-12-13 11:54:09 +0900

    made coverback file

--- coverback.py
+++ coverback.py
@@ -1,22 +1,23 @@
 #! /data/keeling/a/nmd/miniconda3/envs/sage_full/bin/sage-python -u
 
-#SBATCH --array=0-59
+#SBATCH --array=0-199
 #SBATCH --partition m
 #SBATCH --tasks=1
 #SBATCH --mem-per-cpu=4G
 #SBATCH --nice=10000
 #SBATCH --time=7-00:00
-#SBATCH --output=/data/keeling/a/chaeryn2/SLURM_print/OCCcover%A_%a
-#SBATCH --error=/data/keeling/a/chaeryn2/SLURM_error/OCCcover%A_%a
+#SBATCH --output=/data/keeling/a/chaeryn2/SLURM_print/left_OCCcover%A_%a
+#SBATCH --error=/data/keeling/a/chaeryn2/SLURM_error/left_OCCcover%A_%a
 
 import os
 import pickle
 import snappy, regina
-import snappy.twister as twister
 import math
 import time
 import multiprocessing
 import gc
+
+import nscomplex_tg.regina_util
 from normal_surfaces import *
 from itertools import combinations
 from nscomplex_tg import regina_util, surfaces, enumerate_surfaces
@@ -29,9 +30,12 @@ def obviously_compressible(surface):
     S = surfaces.NormalSurface(surface, 0)
     return S.has_obvious_compression()
 
-def detect_totally_geodesic(manifold, index, basename):
+def detect_totally_geodesic(manifold, index):
     M = manifold
-    genus_bd = math.floor(M.volume()/ (8 * 0.29156 * math.pi) + 1)
+    if 'degenerate' in M.solution_type():
+        return
+
+    euler_bd = -math.floor(M.volume()/ (4 * 0.29156 * math.pi))
 
     # use vertex surfaces to enumerate surfaces
 
@@ -39,6 +43,7 @@ def detect_totally_geodesic(manifold, index, basename):
 
     vertex_surfaces = regina.NormalSurfaces(regina.Triangulation3(M), regina.NS_QUAD_CLOSED)  # algHints=regina.NS_VERTEX_DD not needed
     vertex_surfaces_ess = [S for S in vertex_surfaces if not obviously_compressible(S)]
+    vertex_surfaces_vec = [nscomplex_tg.regina_util.extract_vector(S) for S in vertex_surfaces]
 
     tok = time.perf_counter()
 
@@ -47,48 +52,36 @@ def detect_totally_geodesic(manifold, index, basename):
     tik = time.perf_counter()
 
     all_surfaces = []
-    for g in range(2, genus_bd + 1):
-        all_surfaces.extend(find_surfaces(vertex_surfaces_ess, g))
-
-    tok_total = time.perf_counter()
-
-    enumerate_from_vertex_sfces = tok_total - tik
+    for e in range(-1, euler_bd - 1, -1):
+        all_surfaces.extend(find_surfaces(vertex_surfaces_ess, e))
 
     incomp_surfaces = [S for S in all_surfaces if not obviously_compressible(S)]
     incomp_our_surfaces = [from_regina_normal_surface(S, M) for S in incomp_surfaces]
     incomp_vec = [S.get_vector() for S in incomp_our_surfaces]
 
-    find_surface_time = tok_total - tik_total
+    tok_total = time.perf_counter()
 
-    # (might need some time in the future?)
-    # use fundamental surfaces to enumerate surfaces
-    #
-    # tik = time.perf_counter()
-    #
-    # all_surfaces_from_fundamental_dict = enumerate_surfaces.surfaces_to_euler_char(regina.Triangulation3(M), 2 - 2* genus_bd)
-    # all_surfaces_from_fundamental = all_surfaces_from_fundamental_dict.connected_normal(2 - 2 * genus_bd)
-    #
-    # incomp_surfaces_from_fundamental = [S.removeOcts() for S in all_surfaces_from_fundamental if not obviously_compressible(S)]  # list of regina normal surfaces
-    # incomp_our_surfaces_from_fundamental = [from_regina_normal_surface(S, M) for S in incomp_surfaces_from_fundamental]
-    # incomp_vec_from_fundamental = [S.get_vector() for S in incomp_our_surfaces_from_fundamental]
-    #
-    # vec_set = set(incomp_vec_from_fundamental)
-    # incomp_surf_no_duplicate = []
-    # for vec in vec_set:
-    #     incomp_surf_no_duplicate.append(vec_to_NormalSurface(vec, M))
-    #
-    # tok = time.perf_counter()
-    # find_fundamental_surface_time = tok - tik
+    enumerate_from_vertex_sfces = tok_total - tik
 
-    # finding totally geodesic surfaces (using vertex surface enumerations)
+    # finding totally geodesic surfaces
 
     tik = time.perf_counter()
 
     tot_geo_surfaces = []
+    potential_tot_geo_surfaces = []  # these are surfaces that are orientable and whose traces are all real,
+                                     # need to apply steps g, h of Algorithm 2 using nscomplex to check for totally geodesic
+    G = M.fundamental_group(simplify_presentation=False)
     for surface in incomp_our_surfaces:
+        orientable = surface.surface.isOrientable()
         all_real = True
-        gens = surface.fundamental_group_embedding()
-        gens_matrix = [Tietze_to_matrix(gen, M) for gen in gens]
+        if orientable:
+            gens = surface.fundamental_group_embedding()
+        else:
+            vec = S.get_vector()
+            double_vec = tuple(2*x for x in vec)
+            double_surface = vec_to_NormalSurface(double_vec, M)
+            gens = double_surface.fundamental_group_embedding()
+        gens_matrix = [Tietze_to_matrix(gen, G) for gen in gens]
         comb = list(combinations(list(range(len(gens))), 1)) \
                + list(combinations(list(range(len(gens))), 2)) \
                + list(combinations(list(range(len(gens))), 3))
@@ -101,23 +94,27 @@ def detect_totally_geodesic(manifold, index, basename):
             if gen.trace().imag().abs() > 0.001:
                 all_real = False
                 break
-        if all_real:
+        if orientable and all_real:
+            potential_tot_geo_surfaces.append(surface.get_vector())
+        elif not orientable and all_real:
             tot_geo_surfaces.append(surface.get_vector())
 
     tok = time.perf_counter()
 
-    surface_fun_gp_time = tok - tik
+    tot_geo_time = tok - tik
 
     result = {'manifold': regina.Triangulation3(M).tightEncoding(),
-              'runtime_surfaces': find_surface_time,
-              'runtime_gp': surface_fun_gp_time,
-              'all_surfaces': incomp_vec,
-              'tot_geo': tot_geo_surfaces,
+              'manifold_name': M.name(),
               'runtime_vertex_surfaces': find_vertex_sfces,
-              'runtime_enumerating_surfaces_from_vertex_surfaces': enumerate_from_vertex_sfces}
+              'runtime_enumerate_surfaces': enumerate_from_vertex_sfces,
+              'runtime_tot_geo': tot_geo_time,
+              'vertex_surfaces_vec': vertex_surfaces_vec,
+              'all_surfaces_vec': incomp_vec,
+              'tot_geo': tot_geo_surfaces,
+              'potential_tot_geo': potential_tot_geo_surfaces}
 
-    directory = '/data/keeling/a/chaeryn2/tg_computation_outputs/'
-    filename = f'{basename}_{index[0]}_{index[1]}_{index[2]}'
+    directory = '/data/keeling/a/chaeryn2/computation_outputs/'
+    filename = f'cover_info_{index[0]}_{index[1]}_{index[2]}_{M.name()}'
     with open(directory+filename, 'wb') as file:
         pickle.dump(result, file)
 
@@ -127,7 +124,7 @@ if __name__ == '__main__':
     census = snappy.OrientableCuspedCensus()
 
     for M_index, M in enumerate(census):
-        if M_index % 60 == task:
+        if M_index % 200 == task:
             vol = M.volume()
             low_bd = math.ceil(3.6 / vol)
             up_bd = math.floor(20 / vol)
@@ -137,7 +134,7 @@ if __name__ == '__main__':
                     if f'cover_info_{M_index}_{index}_{cover_index}_' not in os.listdir('/data/keeling/a/chaeryn2/computation_outputs/'):
                         gc.collect()
                         p = multiprocessing.Process(target=detect_totally_geodesic,
-                                                    args=(cover, (M_index, index, cover_index), 'totally_geodesic_info_cover'))
+                                                    args=(cover, (M_index, index, cover_index)))
                         p.start()
                         p.join(5000)
                         if p.is_alive():
