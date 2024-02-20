d1ef85321be09a8851bf7ed9ad5ba4f1c78e95c9
chaerynlee <chaeryn2@illinois.edu>
2024-02-20 11:40:52 +0900

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -1,3 +1,14 @@
+#! /data/keeling/a/nmd/miniconda3/envs/sage_full/bin/sage-python -u
+
+#SBATCH --partition m
+#SBATCH --tasks=1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --nice=10000
+#SBATCH --time=7-00:00
+#SBATCH --output=/data/keeling/a/chaeryn2/SLURM_print/data_collection%A_%a
+#SBATCH --error=/data/keeling/a/chaeryn2/SLURM_error/data_collection%A_%a
+
+
 import os
 import regina
 import snappy
