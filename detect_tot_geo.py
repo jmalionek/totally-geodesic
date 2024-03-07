bbeb49165a39bffe17ee6d9ea5188b0d0b707c93
chaerynlee <chaeryn2@illinois.edu>
2024-03-07 12:13:38 +0900

    Update detect_tot_geo.py

--- detect_tot_geo.py
+++ detect_tot_geo.py
@@ -10,13 +10,19 @@ from nscomplex_tg import regina_util, surfaces, enumerate_surfaces
 from sage.all import block_matrix, matrix, vector, CC, FreeGroup
 
 def obviously_compressible(surface):
-    '''
-    surface: regina normal surface
-    '''
+    """
+    Given a Regina NormalSurface object determines whether the surface has an obvious compressing disc.
+    """
     S = surfaces.NormalSurface(surface, 0)
     return S.has_obvious_compression()
 
 def detect_totally_geodesic(manifold, filename=None):
+    """
+    Given a Snappy Manifold object, finds all totally geodesic surfaces and possibly totally geodesic surfaces (surfaces
+    that reach Algorithm2 step g) then pickles relevant information.
+    The name for the resulting pickle file can be given with the filename argument; by default the file will be saved as
+    "info_(name of manifold provided by Snappy)".
+    """
     M = manifold
     if 'degenerate' in M.solution_type():
         return
